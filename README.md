63f60c70976b72eb8d1f9b3dab47533185f4aa13
Akshit Garg <garg.akshit@gmail.com>
2021-07-10 22:49:33 +0530

    feat: add run.mjs and some docs
    Signed-off-by: Akshit Garg <garg.akshit@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # ReLisp
 
 A lisp ([mal](https://github.com/kanaka/mal)) dialect written in ReScript
+
+### Building
+
+- Install the npm dependencies using `yarn`
+- Build the ReScript code using `yarn res:build`
+
+### Running files
+
+You can run a file using `node run.mjs file.relisp`.
+
+### Examples
+
+```clojure
+(def! a (atom 1))
+
+(def! my-fun
+  (fn* [a]
+    (print @a)
+    (reset! a (+ @a 1))
+    (print @a)))
+
+(my-fun)
+```
---
+++ run.mjs
@@ -0,0 +1,24 @@
+import { repl } from "./src/REPL.mjs";
+
+if (process.argv.length === 3) {
+  const fname = process.argv[2];
+  const program = [
+    `(def! load-file
+      (fn* (f)
+        (eval
+         (read-string (str
+             "(do "
+             (slurp f)
+             "nil)")))))`,
+    `(load-file "header.relisp")`,
+    `(load-file "${fname}")`,
+  ];
+
+  const result = repl(undefined, program, [])._0[2];
+  if (result !== "nil") {
+    console.log(result);
+  }
+} else {
+  console.error("Please specify a file name");
+  process.exit(1);
+}
